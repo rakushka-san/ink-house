ab2ab4259ce68c025b1f141bddee621e90f6dd6c
rakushka-san <vladislavmohov@mail.ru>
2023-08-15 16:52:31 +0300

    Инициализация сервера

---
+++ Ink. House Back/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const path = require('path')
+
+const app = express()
+
+app.use(cors)
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const { DB_HOST, DB_PORT, DB_NAME } = process.env
+
+DB_URL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
+mongoose
+	.connect(DB_URL)
+	.then(() => {
+		console.log('Connected to MongoDB')
+	})
+	.catch(err => {
+		console.log(`Error connecting to MongoDB: ${err}`)
+	})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, err => {
+	err
+		? console.log('Error on server start: ', err)
+		: console.log(`Server listening on port ${PORT}`)
+})
