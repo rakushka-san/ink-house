ea0cab0282bf0332a5460a6450c681e11b9d788e
rakushka-san <vladislavmohov@mail.ru>
2023-10-27 03:10:43 +0300

    Добавление роутера и стейт менеджера

--- ink-house-front/src/main.ts
+++ ink-house-front/src/main.ts
@@ -1,7 +1,23 @@
-import './assets/scss/styles.scss'
-import './assets/scss/grid/grid.scss'
+import '@/assets/scss/styles.scss'
+import '@/assets/scss/grid/grid.scss'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from '@/App.vue'
+
+import AppHome from '@/components/AppHome.vue'
+import AppOrder from '@/components/AppOrder.vue'
+
+const routes = [
+  { path: '/', component: AppHome },
+  { path: '/order', component: AppOrder }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
+createApp(App).use(createPinia()).use(router).mount('#app')
